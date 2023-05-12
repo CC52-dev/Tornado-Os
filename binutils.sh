cf79f0e16af141a73b43fdc6ff5f63d95a5f37ac
Eshwar Balaji Yogesh <eshwarb.y@outlook.com>
2023-05-12 17:36:37 -0500

    as

---
+++ binutils.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+./checkmount.sh
+tar -xvf binutils-2.40.tar.xz
+cd binutils-2.40
+mkdir -v build
+cd build
+../configure --prefix=$LFS/tools \
+ --with-sysroot=$LFS \
+ --target=$LFS_TGT \
+ --disable-nls \
+ --enable-gprofng=no \
+ --disable-werror
+make
+make install
+cd ../..
+rm -Rf binutils-2.40
+echo 'Binutils Complete!!!'
